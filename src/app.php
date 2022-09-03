38e0de5add63386c8d9d53d689174d335f574afe
Tammy Robinson <TammyTee@users.noreply.github.com>
2022-09-03 18:07:43 +0000

    Add completed challenge code 01_06

--- src/app.php
+++ src/app.php
@@ -1,3 +1,12 @@
 <?php
 
 include '_includes.php';
+
+$member = new Member();
+$workspace = new Workspace();
+$chat = new Chat();
+$message = new Message();
+
+echonl(get_declared_classes());
+
+echonl($workspace);
