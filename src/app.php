48722f8b1344666814cd1e1cf7f2b3913eca54a3
Tammy Robinson <TammyTee@users.noreply.github.com>
2022-09-06 06:14:00 +0000

    Add completed challenge code 02_03

--- src/app.php
+++ src/app.php
@@ -3,5 +3,23 @@
 include '_includes.php';
 
 $admin = new Member();
+$admin->username = 'acme_admin';
 
 $member = new Member();
+$member->username = 'acme_member';
+
+$workspace = $admin->createWorkspace('acme');
+$admin->addWorkspaceMember($member, $workspace);
+
+echonl($workspace->getUrl());
+echonl($workspace->members);
+
+$chat = $member->createChat('general', $workspace);
+$member->postMessageToChat('Hello!', $chat);
+
+echonl($chat->messages);
+
+$member2 = new Member();
+$member2->username = 'member2';
+
+$member2->createChat('random', $workspace);
