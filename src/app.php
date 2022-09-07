e9a5f820866ea2f0cd12a7ff000ea0e7ef97096b
Tammy Robinson <TammyTee@users.noreply.github.com>
2022-09-07 15:02:24 +0000

    Add completed challenge code 03_03

--- src/app.php
+++ src/app.php
@@ -13,3 +13,17 @@ $admin->addWorkspaceMember($memberB, $workspace);
 
 // creating a chat using the Chat class
 // $chat = $memberA->createChat('general', $workspace);
+
+$channel = $admin->createChannel('example', $workspace);
+$admin->postMessageToChat('Hello', $channel);
+
+$dm = $memberA->createDirectMessage([$memberB], $workspace);
+$memberB->postMessageToChat('Hello', $dm);
+// admin cannot post to this chat
+// $admin->postMessageToChat('Hello', $dm);
+
+echonl($channel->getMessages());
+echonl($dm->getMessages($memberB));
+
+// admin cannot get messages for this chat
+// echonl($dm->getMessages($admin));
